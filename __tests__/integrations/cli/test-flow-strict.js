4475b050807713deffd31a12626c578b71aef1f2
Xandor Schiefer <me@xandor.co.za>
2018-05-15 23:39:19 +0200

    fix: Support '@flow strict-local' pragma

--- __tests__/integrations/cli/test-flow-strict.js
+++ __tests__/integrations/cli/test-flow-strict.js
@@ -11,12 +11,13 @@ const testProjectDir = path.join(FIXTURE_PATH, 'flow-strict');
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000; // 10 second timeout
 
-test('Accept \'@flow strict\' pragma and treat it like \'@flow\' pragma', async () => {
+test('Accept \'@flow strict\' and \'@flow strict-local\' pragmas', async () => {
   const {stdout, stderr} = await runFlowCoverageReport([
     '-i', `"src/*.js"`
   ], {cwd: testProjectDir});
 
-  const filteredStdout = stdout.split('\n').filter(line => line.indexOf('src/main') >= 0);
+  const filteredStdoutMain = stdout.split('\n').filter(line => line.indexOf('src/main') >= 0);
+  const filteredStdoutLocal = stdout.split('\n').filter(line => line.indexOf('src/local') >= 0);
 
-  expect({filteredStdout, stderr}).toMatchSnapshot();
+  expect({filteredStdoutMain, filteredStdoutLocal, stderr}).toMatchSnapshot();
 });
--- src/lib/flow.js
+++ src/lib/flow.js
@@ -142,7 +142,13 @@ export type FlowCoverageJSONData = {
     uncovered_locs: Array<FlowUncoveredLoc>
   },
   filename?: string,
-  annotation?: 'no flow' | 'flow weak' | 'flow' | 'flow strict',
+  // eslint-disable-next-line flowtype/space-after-type-colon
+  annotation?:
+    | 'no flow'
+    | 'flow weak'
+    | 'flow'
+    | 'flow strict'
+    | 'flow strict-local',
   percent: number,
   error?: string,
   isError?: boolean,
@@ -247,7 +253,11 @@ export async function collectFlowCoverageForFile(
     // In strictCoverage mode all files that are not strictly flow
     // (e.g. non annotated and flow weak files) are considered
     // as completely uncovered.
-    if (strictCoverage && ['flow', 'flow strict'].indexOf(parsedData.annotation) === -1) {
+    if (strictCoverage && [
+      'flow',
+      'flow strict',
+      'flow strict-local'
+    ].indexOf(parsedData.annotation) === -1) {
       parsedData.expressions.uncovered_count += parsedData.expressions.covered_count;
       parsedData.expressions.covered_count = 0;
     }
@@ -304,6 +314,7 @@ export function summarizeAnnotations(
     switch (coverageSummaryData.files[filename].annotation) {
       case 'flow':
       case 'flow strict':
+      case 'flow strict-local':
         flowFiles += 1;
         break;
       case 'flow weak':
