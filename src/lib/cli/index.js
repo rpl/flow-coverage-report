9c4a38c5e1f16afbacff44851381011fe2d5489b
Nick Schmidt <nick@kreativgebiet.com>
2018-06-18 10:46:10 +0200

    exit with code 1, when flow is failing

--- src/lib/cli/index.js
+++ src/lib/cli/index.js
@@ -20,7 +20,12 @@ exports.run = () => {
   }
 
   generateFlowCoverageReport({...args}).then(([coverageSummaryData]) => {
-    const {percent, threshold} = coverageSummaryData;
+    const {percent, threshold, flowStatus} = coverageSummaryData;
+
+    if (!flowStatus.passed) {
+      process.exit(1); // eslint-disable-line unicorn/no-process-exit
+    }
+
     if (percent < threshold) {
       console.error(
         `Flow Coverage ${percent}% is below the required threshold ${threshold}%`
