84ef534b69729234aeea071490f7eb08d63126db
Luca Greco <lgreco@mozilla.com>
2018-07-08 05:23:56 +0200

    feat: Added --percent-decimals cli options

---
+++ __tests__/integrations/cli/test-percent-decimals.js
@@ -0,0 +1,24 @@
+'use babel';
+
+import path from 'path';
+
+import {
+  FIXTURE_PATH,
+  runFlowCoverageReport
+} from '../../common';
+
+const testProjectDir = path.join(FIXTURE_PATH, 'project-decimal-coverage');
+
+describe('--percent-decimals option', () => {
+  it('should round percent values using the requested precision', async () => {
+    const {exitCode, error, stderr, stdout} = await runFlowCoverageReport([
+      '--percent-decimals', '2',
+      '-i', '"src/**.js"'
+    ], {cwd: testProjectDir});
+
+    const filteredStdout = stdout.split('\n').filter(
+      line => line.indexOf('src/main.js') >= 0 || line.indexOf('project-decimal-coverage') >= 0);
+
+    expect({exitCode, error, stderr, filteredStdout}).toMatchSnapshot();
+  });
+});
